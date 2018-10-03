dc81207b5fd696dd8d6e891cf08d09b434ff2837
dharmadevops <43808787+dharmadevops@users.noreply.github.com>
2018-10-03 18:26:24 +0530

    Update second.java
    conflitcs

--- second.java
+++ second.java
@@ -1,3 +1,5 @@
 
 this is second java file for checking conflicts on git
 this is my secon java file. for conflit check
+do every this by user only if any conflicts while mergeing resolve it.
+ Then only it will possible to merge.
